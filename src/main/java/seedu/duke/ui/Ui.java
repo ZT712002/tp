9b87f618f86f23deb66b0172baa1a7383a415c1d
zenden <zendentan71@gmail.com>
2025-10-05 14:34:05 +0800

    Remove welcome message

--- src/main/java/seedu/duke/ui/Ui.java
+++ src/main/java/seedu/duke/ui/Ui.java
@@ -36,13 +36,7 @@ public class Ui {
         this.isActive = isActive;
     }
     public void printWelcomeMessage() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("What is your name?");
+        System.out.println("What is your command?");
     }
 
     public void printLineDivider() {
