3f913a1172b39a14d52d77a7134082fde4494688
Andrew Cai <andrewca78@gmail.com>
2025-10-12 23:11:42 +0800

    add policy command parser

---
+++ src/main/java/seedu/duke/parser/PolicyParser.java
@@ -0,0 +1,36 @@
+package seedu.duke.parser;
+
+import seedu.duke.command.AddCommand;
+import seedu.duke.command.Command;
+import seedu.duke.command.DeleteCommand;
+import seedu.duke.command.ListCommand;
+import seedu.duke.exception.FinanceProPlusException;
+
+
+public class PolicyParser extends Parser{
+    private String commandType;
+    private String commandSubtype;
+    private String arguments;
+    public PolicyParser(String type, String commandArgs) {
+        this.commandType = type;
+        String[] commandParts = splitCommand(commandArgs);
+        this.commandSubtype = commandParts[0].toLowerCase();
+        this.arguments = commandParts.length > 1 ? commandParts[1] : "";
+    }
+    @Override
+    protected Command executeAndCreateCommand() throws FinanceProPlusException {
+        String subtype = this.commandSubtype;
+        String args = this.arguments;
+        switch (subtype){
+        case "add":
+            return new AddCommand(commandType, args);
+        case "delete":
+            return new DeleteCommand(commandType, args);
+        case "list":
+            return new ListCommand(commandType);
+        default:
+            throw new FinanceProPlusException("Invalid command subtype, Please try again");
+        }
+    }
+}
+
