a02ec7efade38c450791eb3d4a645aaf140ed538
Andrew Cai <andrewca78@gmail.com>
2025-10-12 23:08:31 +0800

    add policy command into parser

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -24,6 +24,8 @@ public class Parser {
             return new ListParser(commandType, commandArgs);
         case "user":
             return new UserParser(commandType, commandArgs);
+        case "policy":
+            return new PolicyParser(commandType, commandArgs);
         case "exit":
             return new ExitParser();
         default:
