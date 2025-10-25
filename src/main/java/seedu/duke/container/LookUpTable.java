26179a2843842b1e4ce5c1cc82498c584a704609
Andrew Cai <andrewca78@gmail.com>
2025-10-25 21:46:11 +0800

    updated parser to parse task commands

--- src/main/java/seedu/duke/container/LookUpTable.java
+++ src/main/java/seedu/duke/container/LookUpTable.java
@@ -3,21 +3,23 @@ package seedu.duke.container;
 import seedu.duke.client.ClientList;
 import seedu.duke.meeting.MeetingList;
 import seedu.duke.policy.PolicyList;
+import seedu.duke.task.TaskList;
 import seedu.duke.user.UserList;
 
 import java.util.HashMap;
 
 /*
  * A lookup table that maps string keys to their respective list containers.
- * This allows for easy retrieval of different types of lists (e.g., clients, policies, meetings)
+ * This allows for easy retrieval of different types of lists (e.g., clients, policies, meetings, tasks)
  */
 public class LookUpTable {
     private HashMap<String, ListContainer> map;
-    public LookUpTable(ClientList clients, PolicyList policies, MeetingList meetings , UserList user) {
+    public LookUpTable(ClientList clients, PolicyList policies, MeetingList meetings, TaskList tasks, UserList user) {
         map = new HashMap<String, ListContainer>();
         map.put("client", clients);
         map.put("policy", policies);
         map.put("meeting", meetings);
+        map.put("task", tasks);
         map.put("user", user);
     }
     public ListContainer getList(String key) {
