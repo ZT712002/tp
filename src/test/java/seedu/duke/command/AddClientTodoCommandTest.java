473d21f40505c303c3ab8dc0aac7f8cd49cabb55
Andrew Cai <71045679+andrewcai8@users.noreply.github.com>
2025-10-28 18:34:29 +0800

    added archived client list parameter to lookuptable

--- src/test/java/seedu/duke/command/AddClientTodoCommandTest.java
+++ src/test/java/seedu/duke/command/AddClientTodoCommandTest.java
@@ -64,6 +64,7 @@ class AddClientTodoCommandTest {
     static class StubMeetingList extends MeetingList {}
     static class StubTaskList extends TaskList {}
     static class StubUserList extends UserList {}
+    static class StubArchivedClientList extends ArchivedClientList {}
 
     private LookUpTable lookUpTable;
     private SpyClientList spyClientList;
@@ -78,7 +79,7 @@ class AddClientTodoCommandTest {
         spyClientList.setClientToReturn(spyClient);
 
         lookUpTable = new LookUpTable(spyClientList, new StubPolicyList(), new StubMeetingList(),
-                new StubTaskList(), new StubUserList());
+                new StubTaskList(), new StubUserList(), new StubArchivedClientList());
 
         System.setOut(new PrintStream(outContent));
     }
