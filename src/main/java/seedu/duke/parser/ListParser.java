14c38a7ca0a3ea4cb66ce2eaf7a6af53efa3a295
abefqy <abefqy@gmail.com>
2025-10-30 17:44:30 +0800

    Added archived to list parser

--- src/main/java/seedu/duke/parser/ListParser.java
+++ src/main/java/seedu/duke/parser/ListParser.java
@@ -10,7 +10,7 @@ public class ListParser extends Parser{
         String[] commandParts = splitCommand(commandArgs);
         if (commandParts.length > 1){
             throw new FinanceProPlusException("Too many arguments for list command. Please use this format" +
-                    "'list <client/meeting/policy/task>'");
+                    "'list <client/meeting/policy/task/archived>'");
         }
         this.commandSubtype = commandParts[0].toLowerCase();
     }
@@ -21,14 +21,14 @@ public class ListParser extends Parser{
         boolean isValid = checkIsValid(subtype);
         if(!isValid){
             throw new FinanceProPlusException("Invalid list command subtype. Please use this format" +
-                    "'list <client/meeting/policy/task>'");
+                    "'list <client/meeting/policy/task/archived>'");
         }
         return new ListCommand(subtype);
     }
 
     private boolean checkIsValid(String subtype) {
         return subtype.equals("client") || subtype.equals("meeting") || subtype.equals("policy")
-                || subtype.equals("task");
+                || subtype.equals("task") || subtype.equals("archived");
     }
 
 
