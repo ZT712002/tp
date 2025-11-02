568b427d37d549596403f2eff9b48553add638cb
abefqy <abefqy@gmail.com>
2025-11-02 13:58:24 +0800

    Removed "meeting list" test from MeetingParserTest

--- src/test/java/seedu/duke/parser/MeetingParserTest.java
+++ src/test/java/seedu/duke/parser/MeetingParserTest.java
@@ -28,16 +28,36 @@ class MeetingParserTest {
     }
 
     @Test
-    void executeAndCreateCommand_startTimeAfterEndTime_throwsException() {
+    void executeAndCreateCommand_startTimeAfterEndTime_throwsException() throws FinanceProPlusException {
         MeetingParser parser = new MeetingParser("meeting", "add t/Meeting c/Bob d/17-10-2025 from/16:00 to/14:00");
-        Exception exception = assertThrows(FinanceProPlusException.class, parser::executeAndCreateCommand);
+        Command command = parser.executeAndCreateCommand();
+        Exception exception = assertThrows(FinanceProPlusException.class, () -> {
+            command.execute(new seedu.duke.container.LookUpTable(
+                new seedu.duke.client.ClientList(),
+                new seedu.duke.policy.PolicyList(),
+                new seedu.duke.meeting.MeetingList(),
+                new seedu.duke.task.TaskList(),
+                new seedu.duke.user.UserList(),
+                new seedu.duke.client.ArchivedClientList()
+            ));
+        });
         assertEquals("Start time (16:00) must be before end time (14:00)", exception.getMessage());
     }
 
     @Test
-    void executeAndCreateCommand_sameStartAndEndTime_throwsException() {
+    void executeAndCreateCommand_sameStartAndEndTime_throwsException() throws FinanceProPlusException {
         MeetingParser parser = new MeetingParser("meeting", "add t/Meeting c/Bob d/17-10-2025 from/14:00 to/14:00");
-        Exception exception = assertThrows(FinanceProPlusException.class, parser::executeAndCreateCommand);
+        Command command = parser.executeAndCreateCommand();
+        Exception exception = assertThrows(FinanceProPlusException.class, () -> {
+            command.execute(new seedu.duke.container.LookUpTable(
+                new seedu.duke.client.ClientList(),
+                new seedu.duke.policy.PolicyList(),
+                new seedu.duke.meeting.MeetingList(),
+                new seedu.duke.task.TaskList(),
+                new seedu.duke.user.UserList(),
+                new seedu.duke.client.ArchivedClientList()
+            ));
+        });
         assertEquals("Start time (14:00) must be before end time (14:00)", exception.getMessage());
     }
 
@@ -56,12 +76,7 @@ class MeetingParserTest {
         assertEquals("Invalid meeting command arguments", exception.getMessage());
     }
 
-    @Test
-    void executeAndCreateCommand_validList_returnsListCommand() throws FinanceProPlusException {
-        MeetingParser parser = new MeetingParser("meeting", "list");
-        Command command = parser.executeAndCreateCommand();
-        assertInstanceOf(ListCommand.class, command);
-    }
+
 
     @Test
     void executeAndCreateCommand_validForecast_returnsForecastCommand() throws FinanceProPlusException {
